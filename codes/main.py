6356894ca6e771ba8dc6ad9bf4576c1234e4d4f0
SaherNadine <149719057+SaherNadine@users.noreply.github.com>
2025-02-18 23:40:16 +0100

    Update main.py

--- codes/main.py
+++ codes/main.py
@@ -1,4 +1,5 @@
 from code_1 import Sac_a_dos
+from code_2 import Sac_a_dos_dfs
 import pandas as pd
 
 dataset = pd.read_csv('C:\\Users\\sofia\\OneDrive\\Bureau\\ProjetMetaH\\codes\\sac_a_dos_dataset.csv')
@@ -7,3 +8,7 @@ X = dataset.iloc[:,1:].values
 
 sac = Sac_a_dos(X,4,20)
 sac.GA_Algorithm(5,12)
+
+#dfs
+sac_dfs = Sac_a_dos_dfs(X, 20)
+sac_dfs.affich()
